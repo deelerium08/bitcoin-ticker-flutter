dd7feda979a7332d8753a4a42bbd500ed50d86ff
d.florentina.calapod <>
2023-02-15 11:08:39 +0100

    +161. Introducing Cupertino Widgets

--- lib/price_screen.dart
+++ lib/price_screen.dart
@@ -1,4 +1,6 @@
+import 'package:bitcoin_ticker/coin_data.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
 
 class PriceScreen extends StatefulWidget {
   @override
@@ -6,8 +8,32 @@ class PriceScreen extends StatefulWidget {
 }
 
 class _PriceScreenState extends State<PriceScreen> {
+  String selectedCurrency = 'USD';
+
+  List<DropdownMenuItem> getDropdownItems() {
+    List<DropdownMenuItem<String>> dropdownItems = [];
+    for (String currency in currenciesList) {
+      var newItem = DropdownMenuItem(
+        child: Text(currency),
+        value: currency,
+      );
+      dropdownItems.add(newItem);
+    }
+    return dropdownItems;
+  }
+
+  List<Text> getPickerItems() {
+    List<Text> pickerList = [];
+    for (String element in currenciesList) {
+      pickerList.add(Text(element));
+    }
+    return pickerList;
+  }
+
   @override
   Widget build(BuildContext context) {
+    getDropdownItems();
+    getPickerItems();
     return Scaffold(
       appBar: AppBar(
         title: Text('🤑 Coin Ticker'),
@@ -42,10 +68,25 @@ class _PriceScreenState extends State<PriceScreen> {
             alignment: Alignment.center,
             padding: EdgeInsets.only(bottom: 30.0),
             color: Colors.lightBlue,
-            child: null,
+            child: CupertinoPicker(
+              itemExtent: 32.0,
+              onSelectedItemChanged: (selectedIndex) {
+                print(selectedIndex);
+              },
+              children: getPickerItems(),
+            ),
           ),
         ],
       ),
     );
   }
 }
+// DropdownButton<String>(
+//                 value: selectedCurrency,
+//                 items: getDropdownItems(),
+//                 onChanged: (value) {
+//                   setState(() {
+//                     selectedCurrency = value;
+//                     print(selectedCurrency);
+//                   });
+//                 }),
